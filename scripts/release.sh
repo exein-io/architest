f735cd3d6aedc511c9b75cd9f5d8e99a0a0bf5ff
Matteo Nardi <matteo@exein.io>
2023-06-13 11:50:31 +0200

    new(script): improve release script to actually run build process

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,43 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-tempdir="$(mktemp -d)"
-echo "Creating files in $tempdir"
+IMAGES_TO_RELEASE=$(cat <<-END
+  x86_64 5.5
+  x86_64 5.10
+  x86_64 5.13
+  x86_64 5.15
+  x86_64 6.0
+  x86_64 6.3
+  aarch64 5.5
+  aarch64 5.15
+  aarch64 6.2
+  riscv64 6.3
+  mips 5.5
+  mips 6.0
+END
+)
 
 set -e
 
-for i in build/*/images;
+while IFS= read -r line
 do
-  target=$(basename $(dirname "$i"))
-  output="$tempdir/$target.tar.gz"
-  tar czf "$output" "$i"
+  ARCH=$(echo "$line" | cut -f 3 -d\ )
+  KERNEL=$(echo "$line" | cut -f 4 -d\ )
+  echo Bootstrapping $ARCH $KERNEL
+
+  ./scripts/bootstrap.sh $ARCH $KERNEL > /dev/null
+
+  BUILD_DIR="build/${ARCH}_${KERNEL}"
+  LOGFILE="./$BUILD_DIR/.log"
+  (
+    cd "$BUILD_DIR"
+    echo "Compiling $ARCH $KERNEL (log in $LOGFILE)"
+    make >".log" 2>&1 && echo OK || echo FAILURE
+  )
+
+  echo Packing release:
+  mkdir -p build/release/
+  output="build/release/${ARCH}_${KERNEL}.tar.gz"
+  tar czf "$output" -C "$BUILD_DIR/images/" .
   du -sh $output
-done
+  echo
+done <<< "$IMAGES_TO_RELEASE"
